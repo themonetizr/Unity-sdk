47eaa0d0ade2e0c4c18dabab4fee722c969ff801
Rudolfs Agris Stilve <rudolfs@themonetizr.com>
2019-09-14 13:53:51 +0300

    Fixed checkout button still working when disabled

--- Assets/Monetizr/Scripts/UI/Product Page/PressHandler.cs
+++ Assets/Monetizr/Scripts/UI/Product Page/PressHandler.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 namespace Monetizr.UI {
 	public class PressHandler : MonoBehaviour, IPointerDownHandler
 	{
-		[Serializable]
+        private Button _btn;
+
+        private void Start()
+        {
+            _btn = GetComponent<Button>();
+            if(_btn == null)
+            {
+                Debug.LogWarning("PressHandler could not find Button component. Disabling!");
+                enabled = false;
+            }
+        }
+
+        [Serializable]
 		public class ButtonPressEvent : UnityEvent { } 
 
 		public ButtonPressEvent OnPress = new ButtonPressEvent();
 
 		public void OnPointerDown(PointerEventData eventData)
 		{
-			OnPress.Invoke();
+            if(_btn.IsInteractable())
+			    OnPress.Invoke();
 		}
 	}
 }
