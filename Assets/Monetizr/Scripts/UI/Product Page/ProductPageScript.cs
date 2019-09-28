dedf3739d675fc1f3650c48c98821085e451a7f7
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2019-09-28 18:45:57 +0300

    Build issue fixed

--- Assets/Monetizr/Scripts/UI/Product Page/ProductPageScript.cs
+++ Assets/Monetizr/Scripts/UI/Product Page/ProductPageScript.cs
@@ -2,11 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.MemoryProfiler;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
-using UnityEngine.Video;
 
 namespace Monetizr.UI
 {
