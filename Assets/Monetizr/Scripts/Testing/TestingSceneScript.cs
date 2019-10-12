93a3a0064a5ef470fc64bb5da7c2c0b21f30a319
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2019-10-12 15:55:15 +0300

    Improved testing environment

---
+++ Assets/Monetizr/Scripts/Testing/TestingSceneScript.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Monetizr.Testing
+{
+	public class TestingSceneScript : MonoBehaviour
+	{
+		public Text inputText;
+		public InputField tokenField;
+		public InputField productField;
+
+		private void Start()
+		{
+			Application.logMessageReceived += Application_logMessageReceived;
+		}
+
+		private void OnDestroy()
+		{
+			Application.logMessageReceived -= Application_logMessageReceived;
+		}
+
+		public void SetProduct(string p)
+		{
+			productField.text = p;
+		}
+		
+		public void ShowButton()
+		{
+			MonetizrClient.Instance.SetAccessToken(tokenField.text);
+			MonetizrClient.Instance.ShowProductForTag(productField.text);
+		}
+
+		private void Application_logMessageReceived(string condition, string stackTrace, LogType type)
+		{
+			inputText.text += condition;
+			inputText.text += "\n";
+
+			if (inputText.text.Length > 2000)
+			{
+				inputText.text = inputText.text.Substring(inputText.text.Length - 1501, 1500);
+			}
+		}
+	}
+}
