016ec85d8a90f4655f29860d2775cd95e4445207
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-04-08 11:25:55 +0300

    Work on MFP-436, vertical offer list widget

---
+++ Assets/Monetizr/Scripts/UI/Widgets/OfferList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monetizr.UI.Widgets
+{
+	[DefaultExecutionOrder(4672)]
+	public class OfferList : MonoBehaviour
+	{
+		public bool loadOnStart = true;
+		public Transform listHolder;
+		public GameObject template;
+		private List<GameObject> currentItems = new List<GameObject>();
+		private bool _loading = false;
+
+		
+		private void Start()
+		{
+			if(template != null)
+				template.SetActive(false);
+			
+			if(loadOnStart)
+				LoadOffers();
+		}
+		
+		
+
+		public void LoadOffers()
+		{
+			if (_loading) return;
+			if (template == null)
+			{
+				Debug.LogError("<b>MONETIZR: </b>Offer List template is not set");
+				return;
+			}
+			
+			if (listHolder == null)
+			{
+				Debug.LogError("<b>MONETIZR: </b>Offer List list holder is not set");
+				return;
+			}
+
+			_loading = true;
+			MonetizrClient.Instance.AllProducts(UpdateList);
+		}
+
+		public void UpdateList(List<ListProduct> newProducts)
+		{
+			// Clear currently shown offers
+			// Object pooling would be nice, but the tradeoff is not immense here
+			currentItems.ForEach(Destroy);
+			currentItems.Clear();
+
+			if (newProducts == null)
+			{
+				Debug.LogWarning("<b>MONETIZR: </b>Offer list did not load any offers");
+			}
+			else
+			{
+				newProducts.ForEach(p =>
+				{
+					GameObject newItem = Instantiate(template, listHolder, false);
+					var item = newItem.GetComponentInChildren<OfferListItem>();
+					item.Initialize(p);
+					newItem.SetActive(true);
+					currentItems.Add(newItem);
+				});
+			}
+
+			_loading = false;
+		}
+	}
+}
